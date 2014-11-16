804ef77ddd7ecad202e858bb6239e328f0106308
Jorge Aparicio <japaricious@gmail.com>
2014-11-16 09:47:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+[![Build Status][status]](https://travis-ci.org/japaric/onezero.rs)
+
+# `onezero.rs`
+
+Additive and multiplicative identities
+
+# [Documentation][docs]
+
+# License
+
+onezero.rs is dual licensed under the Apache 2.0 license and the MIT license.
+
+See LICENSE-APACHE and LICENSE-MIT for more details.
+
+[docs]: http://japaric.github.io/onezero.rs/onezero/
+[status]: https://travis-ci.org/japaric/onezero.rs.svg?branch=master
---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+//! Additive and multiplicative identities
+
+#![deny(missing_docs, warnings)]
+#![feature(macro_rules)]
+
+/// Multiplicative identity
+pub trait One {
+    /// Returns one
+    fn one() -> Self;
+}
+
+/// Additive identity
+pub trait Zero {
+    /// Returns zero
+    fn zero() -> Self;
+}
+
+macro_rules! float {
+    ($($ty:ty),+) => {$(
+        impl One for $ty { fn one() -> $ty { 1. } }
+        impl Zero for $ty { fn zero() -> $ty { 0. } })+
+    }
+}
+
+float!(f32, f64)
+
+macro_rules! int {
+    ($($ty:ty),+) => {$(
+        impl One for $ty { fn one() -> $ty { 1 } }
+        impl Zero for $ty { fn zero() -> $ty { 0 } })+
+    }
+}
+
+int!(i8, i16, i32, i64, int, u8, u16, u32, u64)
